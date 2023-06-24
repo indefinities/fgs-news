c3b18e48238d68a76dcda6a775ffdfb4f1f1e560
Nat Hsu <natalie.d.hsu@gmail.com>
2023-06-24 15:15:45 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <fw-turn class="magazine" :options="options">
+    <img v-for="i in 31" :key="i" :src="`/pages/p${i}.jpg`"/>
+  </fw-turn>
+</template>
+
+<script lang="js">
+import { FwTurn } from "vue-turnjs";
+
+export default {
+  components: { FwTurn },
+  data() {
+    return {
+      isMobile: false,
+      options: {
+        display: this.isMobile ? "single" : "double",
+        acceleration: true,
+        elevation: 50
+      }
+    }
+  },
+
+  mounted() {
+    this.isMobile = window.matchMedia('max-width: 768px');
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.magazine {
+  width: 100%;
+  height: 95vh;
+
+  img {
+    width: 100%;
+  }
+}
+.magazine .turn-page {
+  background-color: #ccc;
+  background-size: 100% 100%;
+}
+</style>
