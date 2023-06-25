191807f93ae937d8abeb296b721ef7fefc76c097
Nat Hsu <natalie.d.hsu@gmail.com>
2023-06-25 15:37:50 -0400

    fix responsiveness

--- README.md
+++
@@ -1,1 +0,0 @@
-# FGS News
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import App from "./App.vue";
-import { FlippingWidgets } from "vue-turnjs";
-import "vue-turnjs/dist/vue-turnjs.esm.css";
-
-Vue.use(FlippingWidgets);
-new Vue({
-  render: h => h(App)
-}).$mount("#app");
